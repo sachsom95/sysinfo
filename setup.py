f3a595be3034a197a6b759465b86b8f2b3078188
sachsom95 <sachsom@users.noreply.github.com>
2020-01-26 14:47:38 +0000

    1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="systeminfo",
+      version="0.1",
+      description="Basic system information for COMP30830",
+      url="",
+      author="Sachin Soman",
+      author_email="sachin.soman@ucdconnect.ie",
+      licence="GPL3",
+      packages=['systeminfo'],
+      entry_points={
+        'console_scripts':['comp30830_systeminfo=systeminfo.main:main']
+        }
+      )
---
+++ systeminfo/main.py
@@ -0,0 +1,15 @@
+'''
+Created on 22 Jan 2017
+
+@author: aonghus
+'''
+import platform
+
+def main():
+    print("sachin's repo")
+    print("The platform is", platform.platform())
+    return
+
+
+if __name__ == '__main__':
+    main()
